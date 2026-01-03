577d3027c62aa7de77605d6408cc4b515201e878
cj4866759-star <cj4866759@gmail.com>
2026-01-03 03:19:04 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1454910732364812542/eYc89rArTeJ-VGpqHWlS2Xk4BSPghii18O-MURFjfU7Zl34QcP5uansO4EQVaEglBalP'; // Reemplaza esto con tu Webhook real
